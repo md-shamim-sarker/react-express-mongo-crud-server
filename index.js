c05437b1909559ab2bd1f5d7e6c9ed3c5723fa97
shamimspro <shamimspro@gmail.com>
2022-11-03 18:30:10 +0600

    add env and query parameter

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 const express = require('express');
 const cors = require('cors');
-const {MongoClient, ServerApiVersion} = require('mongodb');
+require('dotenv').config();
+const {MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+// middleware
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb://localhost:27017";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.egsefuu.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1});
 
 async function run() {
     try {
-        const database = client.db("userdb");
-        const collection = database.collection("users");
+        const database = client.db("userDb");
+        const collection = database.collection("userCollection");
 
         // C from CRUD
         app.post('/users', async (req, res) => {
@@ -32,16 +34,60 @@ async function run() {
             res.send(users);
         });
 
+        // R from CRUD (find by id)
+        app.get('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const user = await collection.findOne(query);
+            res.send(user);
+        });
+
+        // R from CRUD (using query parameters)
+        // http://localhost:5000/usrs?firstName=Shamim (query parameter format)
+        app.get('/usrs', async (req, res) => {
+            let query = {};
+            if(req.query.firstName) {
+                query = {
+                    firstName: req.query.firstName
+                };
+            }
+            const cursor = collection.find(query);
+            const usrs = await cursor.toArray();
+            res.send(usrs);
+        });
+
+        // U from CRUD
+        app.put('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = {_id: ObjectId(id)};
+            const user = req.body;
+            const option = {upsert: true};
+            const updatedUser = {
+                $set: {
+                    firstName: user.firstName,
+                    lastName: user.lastName
+                }
+            };
+            const result = await collection.updateOne(filter, updatedUser, option);
+            res.send(result);
+        });
+
+        // D from CRUD
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await collection.deleteOne(query);
+            res.send(result);
+        });
+
     } catch(error) {
         console.log(error.message);
     }
 }
 run().catch(console.dir);
 
-
-
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Server is working fine!!!');
 });
 
 app.listen(port, () => {
